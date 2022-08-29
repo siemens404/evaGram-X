2629fe22c3b5f804802765ccf5e66887063ecf1c
IAmDeadlylxrd <contact@deadlylxrd.me>
2022-08-29 21:44:27 +0500

    fix: Repeat message in thread

--- app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
@@ -5269,17 +5269,18 @@ public class MessagesController extends ViewController<MessagesController.Argume
       case R.id.btn_messageRepeat: {
         if (selectedMessage != null) {
           TdApi.Message msg = selectedMessage.getMessage();
-          if (selectedMessage.canBeSaved()) {
+          long msgThreadId = getMessageThreadId();
+          if (selectedMessage.canBeSaved() && msgThreadId == 0L) {
             tdlib.client().send(new TdApi.ForwardMessages(msg.chatId, msg.chatId, new long[]{msg.id}, null, false, false, false), tdlib.messageHandler());
           } else if (selectedMessage instanceof TGMessageText) {
             TdApi.MessageText content = (TdApi.MessageText) msg.content;
-            tdlib.client().send(new TdApi.SendMessage(msg.chatId, 0, 0, null, null, new TdApi.InputMessageText(content.text, content.webPage == null, true)), tdlib.messageHandler());
+            tdlib.client().send(new TdApi.SendMessage(msg.chatId, msgThreadId, 0, null, null, new TdApi.InputMessageText(content.text, content.webPage == null, true)), tdlib.messageHandler());
           } else if (selectedMessage instanceof TGMessageSticker) {
             TdApi.MessageSticker content = (TdApi.MessageSticker) msg.content;
 	    TdApi.Sticker sticker = content.sticker;
 	    TdApi.InputFile inputSticker = (TdApi.InputFile) new TdApi.InputFileLocal(sticker.sticker.local.path);
 	    TdApi.InputThumbnail inputThumbnail = new TdApi.InputThumbnail((TdApi.InputFile) new TdApi.InputFileLocal(sticker.thumbnail.file.local.path), sticker.thumbnail.width, sticker.thumbnail.height);
-            tdlib.client().send(new TdApi.SendMessage(msg.chatId, 0, 0, null, null, new TdApi.InputMessageSticker(inputSticker, inputThumbnail, sticker.width, sticker.height, sticker.emoji)), tdlib.messageHandler());
+            tdlib.client().send(new TdApi.SendMessage(msg.chatId, msgThreadId, 0, null, null, new TdApi.InputMessageSticker(inputSticker, inputThumbnail, sticker.width, sticker.height, sticker.emoji)), tdlib.messageHandler());
           }
           clearSelectedMessage();
         }
